25067f3535db18115f6fda99a364621534a5a1a7
leonid ivanov <leonid.ivanov.w@gmail.com>
2022-11-11 04:47:34 +0500

    Task 60 done

---
+++ Task60/Program.cs
@@ -0,0 +1,19 @@
+﻿int[,,] array = new int[2, 2, 2] {{{12, 43}, {77, 47}}, 
+                                  {{98, 35,}, {86, 53}}};
+System.Console.WriteLine("The array is:");
+PrintArray(array);
+void PrintArray(int[,,] a)
+{
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+        {
+            for (int k = 0; k < a.GetLength(2); k++)
+            {
+                System.Console.Write($"{a[i, j, k]}({i}, {j}, {k}) \t");
+            }
+            System.Console.WriteLine();
+        }
+        System.Console.WriteLine();
+    }
+}
